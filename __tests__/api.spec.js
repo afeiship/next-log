83c06a57dc47802e4430bafb45908d65dba45b93
afeiship <1290657123@qq.com>
2020-11-10 15:07:45 +0800

    fix: add no menaingful test

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -7,6 +7,6 @@ describe('api.basic test', () => {
     const obj2 = { email: '1290657123@qq.com' };
     const result = {};
     nx.log(result, obj1, obj2);
-    expect(result.name, obj1.name).toBe(null);
+    expect(result.name).toBe(undefined);
   });
 });
