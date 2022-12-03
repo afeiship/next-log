ae45c64809bea1a1a9056c3df62fee4f14b99775
afeiship <1290657123@qq.com>
2022-12-03 21:24:42 +0800

    feat: add typings

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+interface NxStatic {
+  log(...args): void;
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 (function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
+  var timestamp = require('time-stamp');
   var log = require('fancy-log');
 
-  nx.log = log;
+  nx.log = function () {
+    var prefix = '[' + timestamp('HH:mm:ss') + ']';
+    var args = nx.slice(arguments);
+    args.unshift(prefix);
+    console.log.apply(console, args);
+  };
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = nx.log;
